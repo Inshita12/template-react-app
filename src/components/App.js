fd81b43b57e3ec222a336bb86ebabe2265ae3452
Inshita Bamba <inshitabamba@gmail.com>
2021-09-20 23:34:37 +0530

    UI

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
 
-import './App.scss';
+
 
 function App() {
   return (
     <div className="App">
-  
+  App
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import App from './App';
+import App from './components/App';
+import { configureStore } from './store';
+const store=configureStore();
+console.log('store',store.getState());
 ReactDOM.render(
   <React.StrictMode>
     <App />
